ea4d88752eee79bd3d52b5d0b8916fc5325f3a40
吕嘉文 <daughlas@gmail.com>
2022-10-10 22:54:54 +0800

    finish parser to gen ast

--- parser.spec.ts
+++ parser.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from 'vitest'
-import { parser } from './parser'
+import { NodeTypes, parser } from './parser'
 import { tokenizer, TokenTypes } from './tokenizer'
 test.skip(
     'parser', () => {
@@ -19,23 +19,23 @@ test.skip(
             type: 'Program',
             body: [
                 {
-                    type: 'CallExpression',
+                    type: NodeTypes.Expression,
                     name: 'add',
                     params: [
                         {
-                            type: 'NumberLiteral',
+                            type: NodeTypes.Number,
                             value: '2'
                         },
                         {
-                            type: 'CallExpression',
+                            type: NodeTypes.Expression,
                             name: 'subtract',
                             params: [
                                 {
-                                    type: 'NumberLiteral',
+                                    type: NodeTypes.Number,
                                     value: '4'
                                 },
                                 {
-                                    type: 'NumberLiteral',
+                                    type: NodeTypes.Number,
                                     value: '2'
                                 }
                             ]
@@ -55,12 +55,96 @@ test('number', () => {
     ]
 
     const ast = {
-        type: 'Program',
+        type: NodeTypes.Root,
         body: [
             {
-                type: 'Number',
+                type: NodeTypes.Number,
                 value: '2'
             }
         ]
     }
+})
+
+test('callExpression', () => {
+    const tokens = [
+        { type: TokenTypes.Paren, value: '(' },
+        { type: TokenTypes.Name, value: 'add' },
+        { type: TokenTypes.Number, value: '2' },
+        { type: TokenTypes.Number, value: '4' },
+        { type: TokenTypes.Paren, value: ')' },
+    ]
+
+    const ast = {
+        type: NodeTypes.Root,
+        body: [
+            {
+                type: NodeTypes.Expression,
+                name: 'add',
+                params: [
+                    {
+                        type: NodeTypes.Number,
+                        value: '2'
+                    },
+                    {
+                        type: NodeTypes.Number,
+                        value: '4'
+                    }
+                ]
+            }
+        ]
+    }
+
+    expect(parser(tokens)).toEqual(ast)
+});
+
+
+test('two callExpression', () => {
+    const tokens = [
+        { type: TokenTypes.Paren, value: '(' },
+        { type: TokenTypes.Name, value: 'add' },
+        { type: TokenTypes.Number, value: '2' },
+        { type: TokenTypes.Number, value: '4' },
+        { type: TokenTypes.Paren, value: ')' },
+        { type: TokenTypes.Paren, value: '(' },
+        { type: TokenTypes.Name, value: 'add' },
+        { type: TokenTypes.Number, value: '1' },
+        { type: TokenTypes.Number, value: '3' },
+        { type: TokenTypes.Paren, value: ')' },
+    ]
+
+    const ast = {
+        type: NodeTypes.Root,
+        body: [
+            {
+                type: NodeTypes.Expression,
+                name: 'add',
+                params: [
+                    {
+                        type: NodeTypes.Number,
+                        value: '2'
+                    },
+                    {
+                        type: NodeTypes.Number,
+                        value: '4'
+                    }
+                ]
+            },
+            {
+                type: NodeTypes.Expression,
+                name: 'add',
+                params: [
+                    {
+                        type: NodeTypes.Number,
+                        value: '1'
+                    },
+                    {
+                        type: NodeTypes.Number,
+                        value: '3'
+                    }
+                ]
+            }
+        ]
+    }
+
+    expect(parser(tokens)).toEqual(ast)
 })
--- parser.ts
+++ parser.ts
@@ -1,5 +1,87 @@
-import { Token } from "./tokenizer";
+import { Token, TokenTypes } from "./tokenizer";
 
+
+export enum NodeTypes {
+    Root,
+    Number,
+    Expression
+}
+
+interface Node {
+    type: NodeTypes;
+}
+
+type BodyElement = NumberNode | CallExpressionNode;
+
+interface RootNode extends Node {
+    body: BodyElement[];
+}
+interface NumberNode extends Node {
+    value: string;
+}
+
+type CallExpressionChildNode = NumberNode | CallExpressionNode;
+
+interface CallExpressionNode extends Node {
+    name: string;
+    params: CallExpressionChildNode[]
+}
+
+function createRootNode(): RootNode {
+    return {
+        type: NodeTypes.Root,
+        body: []
+    }
+}
+
+function createNumberNode(value: string) {
+    return {
+        type: NodeTypes.Number,
+        value: value
+    }
+}
+
+function createCallExpressionNode(name: string): CallExpressionNode {
+    return {
+        type: NodeTypes.Expression,
+        name,
+        params: []
+    }
+}
+
+// TODO 优化下
 export function parser(tokens: Token[]): any {
+    let current = 0;
+    const rootNode = createRootNode();
 
+    function walk() {
+
+        let token = tokens[current];
+        if (token.type === TokenTypes.Number) {
+            current++;
+            return createNumberNode(token.value);
+        }
+
+        if (token.type === TokenTypes.Paren && token.value === '(') {
+            token = tokens[++current];
+            const node = createCallExpressionNode(token.value);
+
+            token = tokens[++current];
+
+            while (!(token.type === TokenTypes.Paren && token.value === ')')) {
+                node.params.push(walk())
+                token = tokens[current];
+            }
+            current++;
+            return node;
+        }
+
+        throw new Error(`无法识别 token ${token}`);
+
+    }
+
+    while (current < tokens.length) {
+        rootNode.body.push(walk());
+    }
+    return rootNode;
 }
